b7e0cf390211053a71965d7e68e65b11cb6650bb
btmxh <ngoduyanh.chip@gmail.com>
2025-04-22 09:57:03 +0700

    feat: add move line up/down keybinds

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -8,3 +8,6 @@ vim.keymap.set('n', '<leader>ph', '<cmd>split<CR>', { desc = 'Open horizontal sp
 vim.keymap.set('n', '<leader>pv', '<cmd>vsplit<CR>', { desc = 'Open vertical split' })
 vim.keymap.set('n', '<leader>pH', '<cmd>split<CR><cmd>term<CR>', { desc = 'Open horizontal terminal split' })
 vim.keymap.set('n', '<leader>pV', '<cmd>vsplit<CR><cmd>term<CR>', { desc = 'Open vertical terminal split' })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
